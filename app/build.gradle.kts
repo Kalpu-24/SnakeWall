089c341f6f8382351a7d72bfa957c10efa0e0c09
Kalpu-24 <22amtics013@gmail.com>
2024-12-21 14:45:14 +0530

    monochrome icon added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "kalp.snake.wall"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0.1"
+        versionCode = 100000
+        versionName = "1.0.0.0.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
